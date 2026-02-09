14aea0d1dd0e5559829c304b68513b23fb71ac48
J-Dizzle661 <jason.alcaraz.411@my.csun.edu>
2026-02-09 11:58:43 -0800

    Base Components complete for models tage
    Just need to add the finishing touches and functionality

--- electron/src/renderer/components/App.jsx
+++ electron/src/renderer/components/App.jsx
@@ -37,7 +37,7 @@ const SettingsIcon = ({TopSettingsOpen, setTopSettingsOpen}) => {
     );
 }
 
-const SearchBar = () => {
+export const SearchBar = () => {
     return (
         <div id = "searchWrapper">
             <div className="searchBarWrapper">
--- electron/src/renderer/components/ModelsTab.jsx
+++ electron/src/renderer/components/ModelsTab.jsx
@@ -1,5 +1,6 @@
 //simple array of the models available, can be changed whenever 
 export const models = ['Random Forest', 'Isolation Forest', 'SVM', 'MLP','Logistic Regression'];
+import magGlass from "./images/magGlass.svg";
 
 //ML models will be treated as objects to track their attributes
 function MLModel(name){
@@ -16,22 +17,48 @@ const MLModels = models.map((model)=> new MLModel(model));
 const modelsListJSX = MLModels.map((model)=>{
     console.log(model.name);
     return (
-        <li id={model.name} className="modelTile" key={model.name}><h1>{model.name}</h1></li>
+            <li id={model.name} className="modelTile" key={model.name}>
+                <div className="modelInfoContainer">
+                    <b>
+                        <h2 className="modelName">{model.name}</h2>
+                    </b>
+                    <div>
+                        <ul className="modelStats" >
+                            <li>Accuracy: {model.accuracy}</li>
+                            <li>Inference Time: {model.inferTime}</li>
+                            <li>F1 Score: {model.f1score}</li>
+                            <li>Total Flows: {model.totFlows}</li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
     );
 });
 
 export const ModelsTab = ()=>{
     return(
         <>
+            <b>
+                <h1 id="modelsTabHeader">Models</h1>
+            </b>
+            <ModelsSearchBar/>
             <Tiles/>
-            <h1>Yeah Buddy!</h1>
         </>
     );
 }
 const Tiles = ()=>{
     return (
-        <ul id="modelTiles">
+        <ul id="modelTilesList">
             {modelsListJSX}
         </ul>
+    );
+}
+
+const ModelsSearchBar = ()=>{
+    return (
+        <div id="innerModelSearch">
+            <object className="magGlass" data={magGlass} type="image/svg+xml"></object>
+            <input type="search" placeholder="Search" id="smallSearchBar"/>
+        </div>
     );
 }
